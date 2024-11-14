de4ed1b6b80de987f115325d509f4c88ce413e10
anouar bakouch <anouar.bakouch@outlook.fr>
2024-11-14 10:45:03 +0100

    Fix : routing to login if not connected

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Login } from './users/Login'; 
+import { Login } from './users/Login';
 import Home from './Home';
-import { Register } from './users/Register'; 
+import { Register } from './users/Register';
+import PrivateRoute from './PrivateRoute'; // Import PrivateRoute
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<PrivateRoute element={<Home />} />} /> {/* Use PrivateRoute for Home */}
       </Routes>
     </Router>
   );
---
+++ src/PrivateRoute.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const PrivateRoute = ({ element }) => {
+  const token = useSelector((state) => state.user.token);
+
+  return token ? element : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
