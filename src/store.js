e7a1c0674c1fe6b6e0536d5478c87429fcd6ebc1
anouar bakouch <anouar.bakouch@outlook.fr>
2024-11-14 10:01:13 +0100

    Redux store

---
+++ src/store.js
@@ -0,0 +1,15 @@
+
+
+import {configureStore} from '@reduxjs/toolkit'
+import userReducer from './features/user/userSlice';
+
+export const store = configureStore({
+
+    reducer : {
+        user : userReducer,
+    },
+
+
+});
+
+export default store;
--- src/users/Login.tsx
+++ src/users/Login.tsx
@@ -3,45 +3,48 @@ import { CustomError } from "../models/CustomError";
 import { Session } from "../models/Session";
 import { loginUser } from "./loginApi";
 import React from "react";
+import "./Login.css";
 
-export function Login () {
+export function Login() {
+  const [error, setError] = useState({} as CustomError);
+  const [session, setSession] = useState({} as Session);
 
-    const [error,setError] = useState({} as CustomError);
-    const [session,setSession] = useState({} as Session);   
+  const HandleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const data = new FormData(form);
 
-    const HandleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-
-        event.preventDefault();
-        const form = event.currentTarget;
-        const data = new FormData(form);
-        
-        loginUser({user_id:-1,username : data.get('login') as string,password : data.get('password') as string}, 
-        (result :Session) => {
-            setSession(result)
-            form.reset()
-            setError(new CustomError(""));
-        } , (loginError : CustomError) => {
-            setError(loginError)
-            setSession({} as Session)
-        });
-
-    }
-
-    return(<>
-        <form onSubmit={HandleSubmit}>
-            <input name="login" placeholder="login"/><br/>
-            <input name="password" placeholder="password"/><br/>
-            <button type="submit">connexion</button>
-        </form>
-            { session.token &&
-                <span>{session.username} : {session.token}</span>
-            }
-            { error.message &&
-                <span>{error.message}</span>
-            }
-        </>
+    loginUser(
+      {
+        user_id: -1,
+        username: data.get("login") as string,
+        password: data.get("password") as string,
+      },
+      (result: Session) => {
+        setSession(result);
+        form.reset();
+        setError(new CustomError(""));
+      },
+      (loginError: CustomError) => {
+        setError(loginError);
+        setSession({} as Session);
+      }
     );
+  };
 
-
-
+  return (
+    <div className="login-container">
+      <form className="login-form" onSubmit={HandleSubmit}>
+        <input name="login" placeholder="login" /><br />
+        <input name="password" type="password" placeholder="password" /><br />
+        <button type="submit">connexion</button>
+      </form>
+      {session.token && (
+        <span className="session-info">
+          {session.username} : {session.token}
+        </span>
+      )}
+      {error.message && <span className="error-message">{error.message}</span>}
+    </div>
+  );
 }
